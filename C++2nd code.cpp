8fe8bbadf6c04db299533e5ab4ed54e879bf8a94
Mohamed <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-28 01:32:00 +0300

    C++2nd code

---
+++ C++2nd code.cpp
@@ -0,0 +1,23 @@
+ #include<iostream>
+    using namespace std;
+    int main()
+    {
+float a,b;
+cout<<"\n\tEnter First Number a : " ;
+cin>>a;
+cout<<" \n\tEnter Second Number b : ";
+cin>>b;
+cout<<"\n\n \t \t *********************\n\n" ;
+cout<<" \n\tYou have Entered a & b = " ;
+cout<<a;
+cout<<" & ";
+cout<< b;
+cout<<"\n";
+cout<< "\n \t The sum of a + b = ";
+cout<<(a+b);
+cout<<" \n ";
+cout<<"\n\t The Average of a & b = ";
+cout<< ((a+b)/2);
+cout<<"\n";
+return 0 ;
+    }

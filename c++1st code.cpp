5408c849e7faa1cc688bbcc8691a0f2e450a1436
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-28 02:16:31 +0300

    Delete c++1st code.cpp

--- c++1st code.cpp
+++
@@ -1,8 +0,0 @@
- #include <iostream>
-    using namespace std; 
-    int main()
-    {
-    cout << "\n \n \n  \t \t  Hello programming World My name is Md salah"<<endl; 
-    system("pause");
-    return 0 ; 
-    }

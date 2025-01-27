1836f480d786dc20458b67a8de509011c0b8073f
Mohamed <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-28 00:54:55 +0300

    Add files via upload
    C++1st code

---
+++ c++1st code.cpp
@@ -0,0 +1,8 @@
+ #include <iostream>
+    using namespace std; 
+    int main()
+    {
+    cout << "\n \n \n  \t \t  Hello programming World My name is Md salah"<<endl; 
+    system("pause");
+    return 0 ; 
+    }

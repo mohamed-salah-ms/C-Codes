49bca8e8c20da84ac23dcf4620910249f351a85b
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-28 18:02:38 +0300

    C++ 7th code (if-->odd&even)

---
+++ C++ 7th code.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{int a;
+cout<<" \n\n\n  \t\t\t  Enter the number : ";
+cin>>a;
+cout<<"\n--------------------------------\n" ;
+if(a%2==0)
+{cout<<" \n\n\t The number is --> Even Number    \n " <<endl;}
+else
+{
+cout<<" \n\n\t The number is  --> odd Number" <<endl;}
+cout<<" \n ---------------------";
+cin.get();
+return 0 ;}

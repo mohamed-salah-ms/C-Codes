2b61e7c4a9e047d21b3184a81d65fa4b353c27a3
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-28 18:00:14 +0300

    C++ 6th (if Statement )

---
+++ C++ 6th code.cpp
@@ -0,0 +1,20 @@
+ #include<iostream>
+    using namespace std; 
+    int main()
+    {
+int a;
+cout<<" Enter the value of a = " ;
+cin>>a; 
+if(a<10)
+{
+cout<<" Welcome in C++ World" ; 
+}
+
+else
+{
+cout<<" Bye Bye"<<endl;
+}
+
+    cin.get();
+return 0; 
+    }

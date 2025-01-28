0b1adc12fb6551a9b2cc7382d8279684f5f23541
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-28 17:45:39 +0300

    C++4th code (The variable label in Memory )

---
+++ C++ 4th code.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+ using namespace std; 
+ int main()
+ {
+ float n ; 
+ cout<<"\n \t \n\t This code to appear variable Memory position in Memory "<<endl ; 
+cout<< "\n \t \n \t Enter any Number   : " ; 
+cin >> n ; 
+cout<<"----------------------------------\n\n \n \t \t \t " ; 
+cout<<" you have Enter the number = " <<&n<<endl; 
+cin.get();
+return 0; 
+}

617c165dd9a6050293d465c1950f005bd062f0c2
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-28 18:13:26 +0300

    C++10th code (Area of circle)

---
+++ C++10th Code.cpp
@@ -0,0 +1,18 @@
+ #include<iostream>
+    #include<cmath>
+    using namespace std; 
+    int main()
+    {
+    float r ; 
+cout<<"\n \t Enter the radius of Circle  : " ; 
+cin>>r; 
+cout<<" \n\n\n \t \t **********************************\n\n\n"; 
+cout<<"\n\n The area of circle is = "; 
+cout<< (M_PI*r*r)<<endl; 
+cout<<"\n\n The area of circle is = ";
+cout<<((M_PI)*pow(r,2));
+cout<<"\n\n The area of circle is = ";
+    cout<<((22/7)*r*r);
+    cin.get();
+return 0 ; 
+    }

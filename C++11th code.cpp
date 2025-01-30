124b58487dcf8459dafbdb5f7970bc76aaa61abe
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-30 12:23:10 +0300

    C++ 11th code(cmath-->abs)

---
+++ C++11th code.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+    #include<cmath>
+    using namespace std; 
+    int main()
+    {
+int x , y ; 
+cout<<" \n \n \t Enter First Number : "; 
+cin>>x; 
+cout<<" \n \n \t Enter Second Number : ";
+cin>>y; 
+cout<<" \n \t *******************************************";
+cout<<" \n \n \t You have Entered x = "; 
+cout<<abs(x);
+cout<<" \n \n \t You have Entered y = ";
+cout<<abs(y);
+cout<<"\n\n\n";
+cin.get();
+return 0 ; 
+    }
+    

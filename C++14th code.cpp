6829a9a9e799632e65890bc8ba173416c10f0467
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-30 12:31:41 +0300

    C++14th Code (Loop using for)

---
+++ C++14th code.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+   using namespace std;
+   int main()
+   {
+double a;
+int b ; 
+cout<<" Enter Number    : ";
+cin>>a;
+cout<<"Enter the number of repeatation : ";
+cin>>b; 
+for(a=0;a<b+1;a++)
+{
+cout<<" The number is -->  "<<a<<endl;
+}
+cin.get();
+return 0 ;
+    }

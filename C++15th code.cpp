540d475a02ecacdf0f72c4d4c7200f603fd49b6e
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-01-30 12:33:40 +0300

    C++15th Code ( cerr & cout)

---
+++ C++15th code.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+    using namespace std; 
+    int main()
+    {
+    cout<<"\n \t \t\t Enter (a)  > (b )\n\n "; 
+float a , b  ; 
+cout<<"\n  \t Enter  the first number a : ";
+cin>> a; 
+cout<< " \n\t Enter The second number b : " ; 
+cin>>b; 
+if (a>b)
+{
+ cout<<" \n The largest number is a = "<<a<<endl;
+}
+else 
+{
+cerr<<"\n \t\t  There is Error "; 
+cout<<"\n \t \t  ther is  error ";
+}
+cin.get();
+return 9 ; 
+    }

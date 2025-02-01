25fb43eb55392b8662029a4654e514a5d8f258b0
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-02-01 21:18:16 +0300

    C++18th code ( start each line with ; ) --> Not clean code

---
+++ C++18th.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+    using namespace std; 
+    int main()
+    {
+cout<<"\n Welcome With Md salah in C++ "
+;cout<<"\n The syntax could be write as this shape"
+;cout<<"\n But this is a bad shape "
+;cout<<"\n So Try To Write  a clean code "<<endl<<endl<<endl; 
+return 0 ;  
+    }

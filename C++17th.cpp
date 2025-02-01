bb6212b2e5c7f0b09c420f24de28a05c329e9362
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-02-01 21:15:39 +0300

    C++17th code--> [Constant (pi) & iomanpi ] with Precision (51)

---
+++ C++17th.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std; 
+int main()
+{
+const  double pi=22.0/7.0;
+cout<<fixed<<setprecision(51); 
+cout<<"\n\t Pi = "<<pi<<endl; 
+cin.get();
+system("pause"); 
+return 0 ; 
+}

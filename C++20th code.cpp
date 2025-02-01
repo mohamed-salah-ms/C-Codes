40bd70d5837d69ea9d74d292a7397c15818c96f0
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-02-01 21:22:25 +0300

    C++20th Code (Constant by #define) -->Not prefere

---
+++ C++20th code.cpp
@@ -0,0 +1,11 @@
+ #include<iostream>
+    using namespace std;
+    #include<iomanip>
+    #define con 3.1415926535897932384626433832795
+    int main()
+    {
+    cout<<fixed<<setprecision(15);
+    cout<<con<<endl;
+    cin.get();
+    return con;
+    }

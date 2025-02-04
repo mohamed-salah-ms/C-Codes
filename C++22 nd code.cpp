5be57921a682bfea654bce4e257b1013995cdd83
Mohamed Salah <36608133+mohamed-salah-ms@users.noreply.github.com>
2025-02-05 00:34:53 +0300

    C++22nd Code--> (the memory size (in bytes) of each variable type using sizeof)

---
+++ C++22 nd code.cpp
@@ -0,0 +1,27 @@
+  #include<iostream>
+    using namespace std; 
+    int main()
+    {
+int x ; 
+float y ; 
+double z; 
+long double l ; 
+cout<<"Enter int number x : ";
+cin>>x; 
+cout<<endl; 
+cout<<"Enter int number y : ";
+cin>>y; 
+cout<<endl; 
+cout<<"Enter int number z : ";
+cin>>z; 
+cout<<endl; 
+    cout<<"Enter int number l : ";
+cin>>l; 
+cout<<endl; 
+cout<<sizeof(x)<<endl;
+cout<<sizeof(y)<<endl;
+cout<<sizeof(z)<<endl;
+cout<<sizeof(l)<<endl;
+cin.get();
+return 0 ; 
+    }
